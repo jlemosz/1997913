3e8f8d06f67c47648705a6c5fddd9fd258a67717
jlemosz <jonatan.lemos@unity3d.com>
2024-11-29 15:53:45 -0500

    Adding new lines to identify the path to Unity.app

--- Assets/Editor/MyCustomBuildProcessor.cs
+++ Assets/Editor/MyCustomBuildProcessor.cs
@@ -13,23 +13,34 @@ public class MyCustomBuildProcessor : IPreprocessBuildWithReport
 
     public void OnPreprocessBuild(BuildReport report)
     {
-        string executablePath;
-        string arguments = "status --header --machinereadable";
+        //string executablePath;
+        //string arguments = "status --header --machinereadable";
 
-        executablePath = System.Environment.GetEnvironmentVariable("PLASTIC_CM_PATH");
+        //executablePath = System.Environment.GetEnvironmentVariable("PLASTIC_CM_PATH");
 	    
         /*
         var plasticConfPath = System.Environment.GetEnvironmentVariable("PROJECT_DIRECTORY");
 	    arguments += $" --clientconf=\"{Path.Combine(plasticConfPath, "client.conf")}\" --tokensconf=\"{ Path.Combine(plasticConfPath, "tokens.conf")}\"";
 	    */
         
+        /*
         var plasticConfPath = Directory.GetCurrentDirectory();
         arguments += $" --clientconf=\"{Path.Combine(plasticConfPath, "client.conf")}\" --tokensconf=\"{Path.Combine(plasticConfPath, "tokens.conf")}\"";
-
+        */
+        
+        var unityPath = System.Environment.GetEnvironmentVariable("UNITY_EXE");
+        Debug.Log($"UNITY.APP PATH = {unityPath}");
+        
+        var currentDirectory = Directory.GetCurrentDirectory();
+        Debug.Log($"CURRENT DIRECTORY = {currentDirectory}");
+        
+        /*
         Debug.Log($"PLASTIC CM PATH = {executablePath}");
         Debug.Log($"PATH TO CONF FILE = {plasticConfPath}");
         Debug.Log($"ARGUMENTS = {arguments}");
+        */
 
+        /*
         var process = Process.Start(new ProcessStartInfo
         {
             FileName = executablePath,
@@ -38,5 +49,6 @@ public class MyCustomBuildProcessor : IPreprocessBuildWithReport
             RedirectStandardError = true,
             UseShellExecute = false
         });
+        */
     }
 }
